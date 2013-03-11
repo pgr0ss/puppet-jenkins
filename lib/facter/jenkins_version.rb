50d45b8e62626344e2bd8cd2a93d3ea0693c445c
Paul Gross <pgross@gmail.com>
2013-03-11 09:18:28 -0700

    Rescue errors in jenkins facter script.

--- lib/facter/jenkins_version.rb
+++ lib/facter/jenkins_version.rb
@@ -1,5 +1,9 @@
 Facter.add("jenkins_version") do
   setcode do
-    Facter::Util::Resolution.exec("dpkg -p jenkins | grep Version | sed 's/Version://'").strip
+    begin
+      Facter::Util::Resolution.exec("dpkg -p jenkins 2>/dev/null | grep Version | sed 's/Version://'").strip
+    rescue
+      nil
+    end
   end
 end
